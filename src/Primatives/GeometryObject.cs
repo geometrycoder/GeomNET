b09c8114a9f61ab3f2d9815930f733a62fbc3939
Geometry Coder <geometry.coder@gmail.com>
2015-02-22 02:42:57 -0500

    Modified GeometryObject

--- src/Primatives/GeometryObject.cs
+++ src/Primatives/GeometryObject.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeomNET {
+namespace GeomNET.Primatives {
 
-    public class GeometryObject {
+    public class GeometryObject : IGeometryObject {
     
     }
 }
