0b38802ceb0d55924dc8b544af240c730ff10005
Geometry Coder <geometry.coder@gmail.com>
2015-02-22 02:11:11 -0500

    Added Point and Vector functionality

--- src/Primatives/Point.cs
+++ src/Primatives/Point.cs
@@ -7,6 +7,71 @@ using System.Threading.Tasks;
 namespace GeomNET.Primatives {
 
     public class Point : GeometryElement {
+        
+        double x, y, z;
 
+        #region constructors
+        public Point() {
+            this.x = 0.0d;
+            this.y = 0.0d;
+            this.x = 0.0d;
+        }
+
+        public Point(double x, double y, double z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        #endregion
+
+        #region operator overloading
+        public static Point operator +(Point p, Vector v) {
+            p.x += v.U;
+            p.y += v.V;
+            p.z += v.W;
+            return p;
+        }
+
+        public static Point operator -(Point p, Vector v) {
+            p.x -= v.U;
+            p.y -= v.V;
+            p.z -= v.W;
+            return p;
+        }
+
+        public static Point operator *(Point p, Vector v) {
+            p.x *= v.U;
+            p.y *= v.V;
+            p.z *= v.W;
+            return p;
+        }
+
+        public static Point operator /(Point p, Vector v) {
+            p.x /= v.U;
+            p.y /= v.V;
+            p.z /= v.W;
+            return p;
+        }
+        #endregion
+
+        #region properties
+        public double X {
+            get { return x; }
+        }
+
+        public double Y {
+            get { return y; }
+        }
+
+        public double Z {
+            get { return z; }
+        }
+        #endregion
+
+        #region methods
+        public double Distance (Point p) {
+            return Math.Sqrt(Math.Pow(x - p.X, 2.0) + Math.Pow(y - p.Y, 2.0) + Math.Pow(z - p.Z, 2.0));
+        }
+        #endregion
     }
 }
--- src/Primatives/Vector.cs
+++ src/Primatives/Vector.cs
@@ -8,5 +8,74 @@ namespace GeomNET.Primatives {
 
     public class Vector : GeometryElement {
 
+        double u, v, w;
+
+        #region constructors
+        public Vector() {
+            this.u = 0.0d;
+            this.v = 0.0d;
+            this.w = 0.0d;
+        }
+
+        public Vector(double u, double v, double w) {
+            this.u = u;
+            this.v = v;
+            this.w = w;
+        }
+        #endregion
+
+        #region operator overloading
+        public static Vector operator +(Vector v1, Vector v2) {
+            v1.x += v2.U;
+            v1.y += v2.V;
+            v1.z += v2.W;
+            return v1;
+        }
+
+        public static Vector operator -(Vector v1, Vector v2) {
+            v1.x -= v2.U;
+            v1.y -= v2.V;
+            v1.z -= v2.W;
+            return v1;
+        }
+
+        public static Vector operator *(Vector v1, Vector v2) {
+            v1.x *= v2.U;
+            v1.y *= v2.V;
+            v1.z *= v2.W;
+            return v1;
+        }
+
+        public static Vector operator /(Vector v1, Vector v2) {
+            v1.x /= v2.U;
+            v1.y /= v2.V;
+            v1.z /= v2.W;
+            return v1;
+        }
+        #endregion
+
+        #region properties
+        public double U {
+            get { return this.u;}
+        }
+
+        public double V {
+            get { return this.v; }
+        }
+
+        public double W {
+            get { return this.w; }
+        }
+        #endregion
+
+        #region methods
+        public Vector CrossProduct {
+
+        }
+
+        public double DotProduct {
+
+        }
+        #endregion
     }
 }
